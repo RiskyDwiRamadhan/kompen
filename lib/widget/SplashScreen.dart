2b380ea99c6d53d88fb6ab900721efe7fcf62989
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2023-11-02 15:49:43 +0700

    membuat splash Screen awal

---
+++ lib/widget/SplashScreen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/scheduler.dart';
+import 'dart:async';
+
+import 'package:kompen/widget/dasboad.dart';
+
+class SplashScreenWidget extends StatefulWidget {
+  const SplashScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenWidget createState() => _SplashScreenWidget();
+}
+
+class _SplashScreenWidget extends State<SplashScreenWidget> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    openSplashScreen();
+  }
+
+  openSplashScreen() async {
+    //bisa diganti beberapa detik sesuai keinginan
+    var durasiSplash = const Duration(seconds: 5);
+
+    return Timer(durasiSplash, () {
+      //pindah ke halaman home
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
+        return DasboadWidget();
+      }));
+    });
+  }
+  // final animationsMap = {
+  //   'containerOnPageLoadAnimation': AnimationInfo(
+  //     trigger: AnimationTrigger.onPageLoad,
+  //     effects: [
+  //       VisibilityEffect(duration: 1.ms),
+  //       FadeEffect(
+  //         curve: Curves.easeInOut,
+  //         delay: 0.ms,
+  //         duration: 400.ms,
+  //         begin: 0,
+  //         end: 1,
+  //       ),
+  //       ScaleEffect(
+  //         curve: Curves.easeInOut,
+  //         delay: 0.ms,
+  //         duration: 400.ms,
+  //         begin: Offset(3, 3),
+  //         end: Offset(1, 1),
+  //       ),
+  //     ],
+  //   ),
+  // };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 36, 35, 35),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+              child: Container(
+            width: double.infinity,
+            height: 500,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFF4B39EF), Color(0xFFEE8B60)],
+                stops: [0, 1],
+                begin: AlignmentDirectional(-1, -1),
+                end: AlignmentDirectional(1, 1),
+              ),
+            ),
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0x00FFFFFF), Colors.white],
+                  stops: [0, 1],
+                  begin: AlignmentDirectional(0, -1),
+                  end: AlignmentDirectional(0, 1),
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    // clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: Image.asset(
+                          'assets/images/polinema_logo.png',
+                        ).image,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+              // ).animateOnPageLoad(animationsMap['containerOnPageLoadAnimation']!),
+              ),
+        ],
+      ),
+    );
+  }
+}
