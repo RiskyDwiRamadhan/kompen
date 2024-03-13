aeda11eb6cf92b4af67c84b52f60f781bbb0f3e3
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2024-03-13 21:08:00 +0700

    setting hosting

--- lib/Service/serviceNetwork.dart
+++ lib/Service/serviceNetwork.dart
@@ -1,12 +1,16 @@
 class serviceNetwork{
   // Leptop
-  //  static const url ='http://192.168.1.200/komspen/';
+  //  static const url ='http://192.168.1.200/kompen/';
   
   // static const url ='http://192.168.213.213/kompen/';
   // static const url ='http://192.168.226.213/kompen/';
   // Hostpot
   // static const url ='http://192.168.108.213/kompen/';
-  static const url ='http://192.168.18.213/kompen/';
+  // static const url ='http://192.168.53.213/kompen/';
+  // static const url ='http://projectkompen.rf.gd/';
+  static const url ='https://projectkompen.000webhostapp.com/';
+  
+
 
   
 
